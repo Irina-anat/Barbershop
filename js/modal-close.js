fc84ed6bff64136e8a9b8f2c915098f78d4e52c5
Iryna Rozmaita <iriszechka@ukr.net>
2024-03-15 19:48:39 +0200

    + some adaptive styles. + change modal-close.js

--- js/modal-close.js
+++ js/modal-close.js
@@ -1,28 +1,25 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    modalLinks: document.querySelectorAll(".modal__list a"), 
+    onlineBookingLink: document.querySelector(".modal__link-btn"), 
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
   window.addEventListener("keydown", onEscKeyPress);
+
+  refs.modalLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      toggleModal();
+    });
+  });
+
+  refs.onlineBookingLink.addEventListener("click", () => {
+    toggleModal();
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
@@ -34,3 +31,29 @@
     }
   }
 })();
+
+
+
+
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+//   window.addEventListener("keydown", onEscKeyPress);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+
+//   function onEscKeyPress(event) {
+//     if (event.code === "Escape") {
+//       toggleModal();
+//     }
+//   }
+// })();
