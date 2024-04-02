d17976fa2ead48d3f0ea6c05461ea00def384606
Iryna Rozmaita <iriszechka@ukr.net>
2024-04-02 13:11:43 +0300

    update animation

--- js/aos-animation.js
+++ js/aos-animation.js
@@ -1,3 +1,22 @@
-AOS.init({ duration: 500, once: true });
+AOS.init({ duration: 600,
+    offset: 200,
+    once: false, 
+    disableMutationObserver: true  });
+AOS.refreshHard();
+
+// function checkWindowSize() {
+//     if (window.innerWidth <= 768) { 
+//         AOS.refresh(); 
+//         AOS.init({ offset: 200 }); 
+//     } else {
+//         AOS.refresh(); 
+//         AOS.init({ offset: 300 }); 
+//     }
+// }
+
+// checkWindowSize();
+
+// window.addEventListener('resize', checkWindowSize);
+
 
 
